5f8d6c808f7b97d67965aa939d81b2cedd3ee024
afeiship <afeiship@users.noreply.github.com>
2026-01-26 13:23:31 +0000

    deploy: 2dcaf54460e64b8b4949aa71d4a8f9d8955bbf0a

---
+++ docs__index.md.902a432e.async.js
@@ -0,0 +1,17 @@
+"use strict";(self.webpackChunkdumi_docs=self.webpackChunkdumi_docs||[]).push([[935],{86131:function(u,n,e){e.r(n);var o=e(29131),m=e(91088),r=e(51114),c=e(27378),p=e(44278),t=e(13047),h=e(40850),x=e(38271),s=e(57337),l=e(78522),i=e(75271),d=e(4676),_=e(52676);function a(){return(0,_.jsx)(s.dY,{children:(0,_.jsx)(i.Suspense,{fallback:(0,_.jsx)(l.Z,{}),children:(0,_.jsx)(_.Fragment,{children:(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsxs)("h2",{id:"installation",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#installation",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Installation"]}),(0,_.jsx)(t.Z,{lang:"bash",children:d.texts[0].value}),(0,_.jsxs)("h2",{id:"quick-start",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#quick-start",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Quick Start"]}),(0,_.jsx)(t.Z,{lang:"bash",children:d.texts[1].value}),(0,_.jsxs)("h2",{id:"documentation",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#documentation",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Documentation"]}),(0,_.jsxs)("ul",{children:[(0,_.jsxs)("li",{children:[(0,_.jsx)(s.rU,{to:"/guide/getting-started",children:d.texts[2].value}),d.texts[3].value]}),(0,_.jsxs)("li",{children:[(0,_.jsx)(s.rU,{to:"/guide/configuration",children:d.texts[4].value}),d.texts[5].value]}),(0,_.jsxs)("li",{children:[(0,_.jsx)(s.rU,{to:"/guide/deployment",children:d.texts[6].value}),d.texts[7].value]})]}),(0,_.jsx)(t.Z,{})]})})})})}n.default=a},4676:function(u,n,e){e.r(n),e.d(n,{texts:function(){return o}});const o=[{value:`# Clone this template
+git clone https://github.com/afeiship/dumi-docs.git
+
+# Install dependencies
+npm install
+
+# Start development server
+npm run dev
+`,paraId:0,tocIndex:0},{value:`# Start development server
+npm run dev
+
+# Build for production
+npm run build
+
+# Preview production build
+npm run preview
+`,paraId:1,tocIndex:1},{value:"Guide",paraId:2,tocIndex:2},{value:" - Get started with the template",paraId:3,tocIndex:2},{value:"Configuration",paraId:4,tocIndex:2},{value:" - Customize your documentation site",paraId:3,tocIndex:2},{value:"Deployment",paraId:5,tocIndex:2},{value:" - Deploy your documentation site",paraId:3,tocIndex:2},{value:"",paraId:6,tocIndex:2}]}}]);
