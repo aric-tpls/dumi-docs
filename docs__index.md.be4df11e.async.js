54c4e3bedab50e938eedf9ad247c4d8dfaefec11
afeiship <afeiship@users.noreply.github.com>
2026-02-01 01:09:35 +0000

    deploy: 2da37c963c789f886c1b7921712546adc3b448fd

---
+++ docs__index.md.be4df11e.async.js
@@ -0,0 +1,19 @@
+"use strict";(self.webpackChunkdumi_docs=self.webpackChunkdumi_docs||[]).push([[935],{86131:function(l,n,e){e.r(n);var u=e(29131),r=e(91088),m=e(51114),c=e(27378),p=e(44278),t=e(13047),h=e(40850),x=e(38271),s=e(57337),a=e(78522),o=e(75271),d=e(4676),_=e(52676);function i(){return(0,_.jsx)(s.dY,{children:(0,_.jsx)(o.Suspense,{fallback:(0,_.jsx)(a.Z,{}),children:(0,_.jsx)(_.Fragment,{children:(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsxs)("h2",{id:"installation",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#installation",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Installation"]}),(0,_.jsx)(t.Z,{lang:"bash",children:d.texts[0].value}),(0,_.jsxs)("h2",{id:"quick-start",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#quick-start",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Quick Start"]}),(0,_.jsx)(t.Z,{lang:"bash",children:d.texts[1].value}),(0,_.jsxs)("h2",{id:"tailwind-demo",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#tailwind-demo",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Tailwind Demo"]}),(0,_.jsxs)("div",{className:"debug x-5 p-2 hover:debug-blue rounded bg-slate-100",children:[d.texts[2].value,(0,_.jsx)("button",{className:"bg-blue-500 text-white px-4 py-2 rounded-md",children:d.texts[3].value}),d.texts[4].value,(0,_.jsx)("button",{className:"bg-red-200 px-4 py-2 rounded-md",children:d.texts[5].value})]}),(0,_.jsxs)("h2",{id:"documentation",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#documentation",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Documentation"]}),(0,_.jsxs)("ul",{children:[(0,_.jsxs)("li",{children:[(0,_.jsx)(s.rU,{to:"/guide/getting-started",children:d.texts[6].value}),d.texts[7].value]}),(0,_.jsxs)("li",{children:[(0,_.jsx)(s.rU,{to:"/guide/configuration",children:d.texts[8].value}),d.texts[9].value]}),(0,_.jsxs)("li",{children:[(0,_.jsx)(s.rU,{to:"/guide/deployment",children:d.texts[10].value}),d.texts[11].value]})]}),(0,_.jsx)(t.Z,{})]})})})})}n.default=i},4676:function(l,n,e){e.r(n),e.d(n,{texts:function(){return u}});const u=[{value:`# Clone this template
+git clone https://github.com/afeiship/dumi-docs.git
+
+# Install dependencies
+npm install
+
+# Start development server
+npm run dev
+`,paraId:0,tocIndex:0},{value:`# Start development server
+npm run dev
+
+# Build for production
+npm run build
+
+# Preview production build
+npm run preview
+`,paraId:1,tocIndex:1},{value:`
+  `,paraId:2},{value:"Get Started",paraId:2},{value:`
+  `,paraId:2},{value:"Show Demo",paraId:2},{value:"Guide",paraId:3,tocIndex:3},{value:" - Get started with the template",paraId:4,tocIndex:3},{value:"Configuration",paraId:5,tocIndex:3},{value:" - Customize your documentation site",paraId:4,tocIndex:3},{value:"Deployment",paraId:6,tocIndex:3},{value:" - Deploy your documentation site",paraId:4,tocIndex:3},{value:"",paraId:7,tocIndex:3}]}}]);
