aa2a38ac5c25b516cfadb27425e96ef97ebff619
afeiship <1290657123@qq.com>
2026-04-02 19:38:12 +0800

    style(tailwind): update content configuration to include TSX files
    - change the content source glob to include .tsx files
    - ensures Tailwind purges styles from component files
    - improves the build process by scanning for utility usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('@jswork/presets-tailwind')],
-  content: ['./docs/**/*.{md,mdx}'],
+  content: ['./docs/**/*.{md,mdx,tsx}'],
   theme: {
     extend: {},
   },
